e43f4e6fa0bcda8ada3ce6428d6df05c838516fe
Muhammad Naufal Hilmy Makarim <40902670+mnaufalhilmym@users.noreply.github.com>
2024-09-02 05:26:19 +0700

    Merge dev to v2 (#2)
    * feat: show driver_incentive in get booking endpoint
    
    * feat: preloading in get list bookings endpoint
    
    * chore: update README.md
    
    * feat: add membership_id if request.membership_id exists
    
    * feat: finalize app
    
    * feat: add deleted_at in drivers and driver_incentives

--- internal/entity/driver_incentive_entity.go
+++ internal/entity/driver_incentive_entity.go
@@ -1,9 +1,12 @@
 package entity
 
+import "gorm.io/gorm"
+
 type DriverIncentive struct {
-	ID        int     `gorm:"column:id;primaryKey"`
-	BookingID int     `gorm:"column:booking_id;unique"`
-	Incentive float64 `gorm:"column:incentive"`
+	ID        int            `gorm:"column:id;primaryKey"`
+	BookingID int            `gorm:"column:booking_id;unique"`
+	Incentive float64        `gorm:"column:incentive"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
 
 	Booking Booking `gorm:"foreignKey:booking_id;references:id"`
 }
